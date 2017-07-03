b5dd47563fb71e74f35b3759edc1d17cb90f10be
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2017-07-03 17:50:19 +0200

    rdf: update test helper to use exported function

--- rdf/memory/graph_test.go
+++ rdf/memory/graph_test.go
@@ -3,7 +3,6 @@ package memory
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"reflect"
 	"sort"
 	"testing"
@@ -169,43 +168,8 @@ func TestDecode(t *testing.T) {
 		t.Error(diff)
 	}
 }
-
-func decodeGraph(d *rdf.Decoder) (*Graph, error) {
-	g := NewGraph()
-	bnodeTriples := make(map[rdf.BlankNode][]rdf.Triple)
-
-	for tr, err := d.Decode(); err != io.EOF; tr, err = d.Decode() {
-		if err != nil {
-			return g, err
-		}
-		switch t := tr.Subject.(type) {
-		case rdf.BlankNode:
-			bnodeTriples[t] = append(bnodeTriples[t], tr)
-			continue
-		}
-		switch t := tr.Object.(type) {
-		case rdf.BlankNode:
-			bnodeTriples[t] = append(bnodeTriples[t], tr)
-			continue
-		}
-		if _, err := g.Insert(tr); err != nil {
-			return nil, err
-		}
-	}
-
-	// Insert triples with bnodes in batches by ID, so that they get assigned
-	// the same (new) blank node ID in the Graph
-	for _, trs := range bnodeTriples {
-		if _, err := g.Insert(trs...); err != nil {
-			return nil, err
-		}
-	}
-	return g, nil
-}
-
 func mustDecode(s string) *Graph {
-	dec := rdf.NewDecoder(bytes.NewBufferString(s))
-	g, err := decodeGraph(dec)
+	g, err := NewFromNTriples(bytes.NewBufferString(s))
 	if err != nil {
 		panic("mustDecode: " + err.Error())
 	}
