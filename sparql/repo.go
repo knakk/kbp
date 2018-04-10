887f26a90e65cc56e7f3fa6c9193f995a627753d
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2018-04-10 18:06:40 +0200

    sparl: various improvements

--- sparql/repo.go
+++ sparql/repo.go
@@ -20,8 +20,9 @@ import (
 // Repo represent a RDF repository, assumed to be
 // queryable via the SPARQL protocol over HTTP.
 type Repo struct {
-	endpoint string
-	client   *http.Client
+	endpoint   string
+	client     *http.Client
+	retryOn404 bool
 }
 
 // NewRepo creates a new representation of a RDF repository. It takes a
@@ -61,25 +62,25 @@ func Timeout(t time.Duration) func(*Repo) error {
 	}
 }
 
-// Select performs a SPARQL HTTP request to the Repo, and returns the
-// parsed application/sparql-results+json response.
-func (r *Repo) Select(q string) (*Results, error) {
-	form := url.Values{}
-	form.Set("query", q)
-	b := form.Encode()
+// RetryOn404 will retry request when response status is 404. (This happens when
+// virtuoso is performing checkpoints.)
+func RetryOn404() func(*Repo) error {
+	return func(r *Repo) error {
+		r.retryOn404 = true
+		return nil
+	}
+}
 
-	// TODO make optional GET or Post, Query() should default GET (idempotent, cacheable)
-	// maybe new for updates: func (r *Repo) Update(q string) using POST?
+func (r *Repo) qselect(body string, n int) (*http.Response, error) {
 	req, err := http.NewRequest(
 		"POST",
 		r.endpoint,
-		bytes.NewBufferString(b))
+		bytes.NewBufferString(body))
 	if err != nil {
 		return nil, err
 	}
-
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Content-Length", strconv.Itoa(len(b)))
+	req.Header.Set("Content-Length", strconv.Itoa(len(body)))
 	req.Header.Set("Accept", "application/sparql-results+json")
 	req.Header.Set("Accept-Encoding", "gzip")
 
@@ -87,6 +88,29 @@ func (r *Repo) Select(q string) (*Results, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	const maxTries = 5
+	if resp.StatusCode == http.StatusNotFound && r.retryOn404 && n <= maxTries {
+		resp.Body.Close()
+		time.Sleep(time.Duration(100*n) * time.Millisecond)
+		return r.qselect(body, n+1)
+	}
+
+	return resp, err
+}
+
+// Select performs a SPARQL HTTP request to the Repo, and returns the
+// parsed application/sparql-results+json response.
+func (r *Repo) Select(q string) (*Results, error) {
+	form := url.Values{}
+	form.Set("query", q)
+	b := form.Encode()
+
+	resp, err := r.qselect(b, 1)
+	if err != nil {
+		return nil, err
+	}
+
 	defer resp.Body.Close()
 	body, err := gzip.NewReader(resp.Body)
 	if err != nil {
@@ -103,7 +127,7 @@ func (r *Repo) Select(q string) (*Results, error) {
 				msg = "Response body: \n" + string(b)
 			}
 		}
-		return nil, fmt.Errorf("Query: SPARQL request failed: %s. "+msg, resp.Status)
+		return nil, fmt.Errorf("Select: SPARQL request failed: %s. "+msg, resp.Status)
 	}
 	results, err := ParseJSON(body)
 	if err != nil {
@@ -111,6 +135,65 @@ func (r *Repo) Select(q string) (*Results, error) {
 	}
 
 	return results, nil
+}
+
+// Update performs a SPARQL HTTP request to the Repo
+func (r *Repo) Update(q string) error {
+	form := url.Values{}
+	form.Set("query", q)
+	b := form.Encode()
+
+	resp, err := r.construct(b, 1)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, err := gzip.NewReader(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		b, err := ioutil.ReadAll(body)
+		var msg string
+		if err != nil {
+			msg = "Failed to read response body"
+		} else {
+			if strings.TrimSpace(string(b)) != "" {
+				msg = "Response body: \n" + string(b)
+			}
+		}
+		return fmt.Errorf("Update: SPARQL request failed: %s. "+msg, resp.Status)
+	}
+	return nil
+}
+
+func (r *Repo) construct(body string, n int) (*http.Response, error) {
+	req, err := http.NewRequest(
+		"POST",
+		r.endpoint,
+		bytes.NewBufferString(body))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(body)))
+	req.Header.Set("Accept-Encoding", "gzip")
+
+	resp, err := r.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	const maxTries = 5
+	if resp.StatusCode == http.StatusNotFound && r.retryOn404 && n <= maxTries {
+		resp.Body.Close()
+		time.Sleep(time.Duration(100*n) * time.Millisecond)
+		return r.construct(body, n+1)
+	}
+
+	return resp, err
 }
 
 // Construct performs a SPARQL HTTP request to the Repo, and returns the
@@ -121,22 +204,11 @@ func (r *Repo) Construct(q string) (*memory.Graph, error) {
 	form.Set("format", "text/plain")
 	b := form.Encode()
 
-	req, err := http.NewRequest(
-		"POST",
-		r.endpoint,
-		bytes.NewBufferString(b))
+	resp, err := r.construct(b, 1)
 	if err != nil {
 		return nil, err
 	}
 
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Set("Content-Length", strconv.Itoa(len(b)))
-	req.Header.Set("Accept-Encoding", "gzip")
-
-	resp, err := r.client.Do(req)
-	if err != nil {
-		return nil, err
-	}
 	defer resp.Body.Close()
 	body, err := gzip.NewReader(resp.Body)
 	if err != nil {
